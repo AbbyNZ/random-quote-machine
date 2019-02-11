cad7b8c8b7f13b2f6dc4035574bc29ed18a83197
AbbyNZ <28561684+AbbyNZ@users.noreply.github.com>
2019-02-11 21:23:19 +1000

    Edit some functions. The project is now working.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
-const projectName = "random-quote-machine";
-
-localStorage.setItem('example_project', 'Random Quote Machine');
+let quotesData;
 
 function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
 
-let quotesData;
-var currentQuote = '', currentAuthor = '';
-
 function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
+
+var currentQuote = '', currentAuthor = '';
 
 function getQuotes() {
   return $.ajax({
     headers: {
       Accept: "application/json"
-    }, url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    },
+    url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
     success: function(jsonQuotes) {
-      if(typeof jsonQuotes === 'string') {
+      if (typeof jsonQuotes === 'string') {
         quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
-        console.log(quotesData);
+		  console.log(quotesData);
       }
     }
   });
@@ -31,23 +28,46 @@ function getRandomQuote() {
 }
 
 function getQuote() {
+
   let randomQuote = getRandomQuote();
   
   currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
 
-currentAuthor = randomQuote.author;
+  if(inIframe())
+  {
+    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+  }
+  
+  $(".quote-text").animate(
+    { opacity: 0 },
+    500,
+    function() {
+      $(this).animate({ opacity: 1}, 500);
+      $("#text").text(currentQuote);
+    }
+  );
+
+  $(".quote-author").animate(
+    { opacity: 0 },
+    500,
+    function() {
+      $(this).animate({ opacity: 1}, 500);
+      $("#author").html(currentAuthor);
+    }
+  );
 }
 
 $(document).ready(function() {
   getQuotes().then(() => {
     getQuote();
   });
-  
+
   $('#new-quote').on('click', getQuote);
-  
+
   $('#tweet-quote').on('click', function() {
     if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('""' + currentQuote + '""' + currentAuthor));
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
     }
   });
 });
